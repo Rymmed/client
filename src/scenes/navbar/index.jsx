057de9c0288a115087cef2e536bc7a6c1fe4e6af
RymMohamed <mhamedrim921@gmail.com>
2023-11-27 00:47:07 +0100

    User&Post

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,32 +1,32 @@
 import { useState } from "react";
 import {
-     Box , 
-    IconButton, 
-    InputBase, 
-    Typography, 
-    Select, 
-    MenuItem,
-    FormControl,
-    useTheme,
-    useMediaQuery
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
 import {
-    Search,
-    Message,
-    DarkMode,
-    LightMode,
-    Notifications,
-    Help,
-    Menu,
-    Close
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
 } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
-import { setMode, setLogout} from "state";
+import { setMode, setLogout } from "state";
 import { useNavigate } from "react-router-dom";
 import FlexBetween from "components/FlexBetween";
 
 const Navbar = () => {
-    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
@@ -39,6 +39,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
+  console.log("User:", user);
   const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
